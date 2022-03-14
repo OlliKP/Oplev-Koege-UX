da1b23a30cd01adbaa28735ef8071e3e84974ab4
Oliver Krumbæk Perving <Oliv535v@edu.zealand.dk>
2022-03-14 11:10:54 +0100

    Første forsøg på Javascript prototype

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let diningPlaces = [
+    {name: "Restaurant Arken", type: "adult"},
+    {name: "Café Vivaldi", type: "cafe"},
+    {name: "Restaurant Bone's", type: "family"},
+    {name: "Restaurant Tacos Locos", type: "family"},
+    {name: "Café Vinbar", type: "cafe"},
+    {name: "Restaurant Malt", type: "adult"},
+]
+
+const ul = document.querySelector('.sort-slider');
+
